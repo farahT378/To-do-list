b4551aa9606e9bcfa8c642f25f8bf2bdd81f4e2f
farah <farahtawfik14@gmail.com>
2023-08-17 12:46:50 +0300

    new proj

---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+    <Task  :key="task.id" v-for="task in tasks" :task="task" @toggle-reminder="$emit('toggle-reminder',task.id)" @delete-task="$emit('delete-task',task.id)" />
+</template>
+
+<script>
+import Task from './Task.vue'
+export default{
+    name:'Tasks',
+    props:{
+       tasks:Array, 
+    },
+    components:{
+        Task,
+    },
+    emits: ['delete-task' , 'toggle-reminder'],
+}
+</script>
